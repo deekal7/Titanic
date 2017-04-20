48fbdf0874940961cd01371201aeb3ae3dac866a
deeptiKalasapura <kalasapura@gmail.com>
2017-04-20 20:32:39 +0530

    lm for age and fare

--- titanic.R
+++ titanic.R
@@ -8,17 +8,48 @@ titanic.test$IsTrainSet <- FALSE
 
 titanic.test$Survived <- NA
 
-#clear missing values of embarked
 titanic.full <- rbind(titanic.train, titanic.test)
+
+#clean missing values of embarked
 titanic.full[titanic.full$Embarked== '',"Embarked"] <- 'S'
 
-#clear missing values of age
-age.median <- median(titanic.full$Age, na.rm = TRUE)
-titanic.full[is.na(titanic.full$Age),"Age"] <- age.median
+#clean missing values of age
+upperage.whisker <- boxplot.stats(titanic.full$Age)$stats[5]
+outlierage.filter <- titanic.full$Age<upperage.whisker
+titanic.full[outlierage.filter,]
+age.equation = "Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked"
+age.model <- lm(
+  formula = age.equation,
+  data = titanic.full[outlierage.filter,]
+)
 
-#clear missing values of fare
-fare.median <- median(titanic.full$Fare, na.rm = TRUE)
-titanic.full[is.na(titanic.full$Fare), "Fare"] <- fare.median
+age.row <- titanic.full[
+  is.na(titanic.full$Age),
+  c("Pclass", "Sex", "SibSp", "Parch", "Fare", "Embarked")
+  ]
+
+age.predictions <- predict(age.model, newdata = age.row )
+titanic.full[is.na(titanic.full$Age), "Age"] <- age.predictions
+
+
+#clean missing values of fare
+upper.whisker <- boxplot.stats(titanic.full$Fare)$stats[5]
+outlier.filter <- titanic.full$Fare<upper.whisker
+titanic.full[outlier.filter,]
+fare.equation = "Fare ~ Pclass + Sex + Age + SibSp + Parch + Embarked"
+fare.model <- lm(
+  formula = fare.equation,
+  data = titanic.full[outlier.filter,]
+)
+
+fare.row <- titanic.full[
+  is.na(titanic.full$Fare),
+  c("Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked")
+]
+
+fare.predictions <- predict(fare.model, newdata = fare.row )
+titanic.full[is.na(titanic.full$Fare), "Fare"] <- fare.predictions
+
 
 #categorical casting
 titanic.full$Pclass <- as.factor(titanic.full$Pclass)
@@ -33,7 +64,7 @@ titanic.train$Survived <- as.factor(titanic.train$Survived)
 
 survived.equation <- "Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
 survived.formula <- as.formula(survived.equation)
-install.packages("randomForest")
+#install.packages("randomForest")
 library(randomForest)
 
 titanic.model <- randomForest(formula = survived.formula, data = titanic.train, ntree= 500, mtry = 3, nodesize = 0.01 * nrow(titanic.train))
@@ -45,4 +76,4 @@ PassengerId <- titanic.test$PassengerId
 output.df <- as.data.frame(PassengerId)
 output.df$Survived <- Survived
 
-write.csv(output.df, file = "kaggle_submission.csv", row.names = FALSE)
+write.csv(output.df, file = "kaggle_submission_2.csv", row.names = FALSE)
