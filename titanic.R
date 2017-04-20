315e19b02d7719ae05b3cc4a6c5e6746705d734b
deeptiKalasapura <kalasapura@gmail.com>
2017-04-20 19:28:29 +0530

    change in R script

--- titanic.R
+++ titanic.R
@@ -8,5 +8,41 @@ titanic.test$IsTrainSet <- FALSE
 
 titanic.test$Survived <- NA
 
+#clear missing values of embarked
 titanic.full <- rbind(titanic.train, titanic.test)
+titanic.full[titanic.full$Embarked== '',"Embarked"] <- 'S'
 
+#clear missing values of age
+age.median <- median(titanic.full$Age, na.rm = TRUE)
+titanic.full[is.na(titanic.full$Age),"Age"] <- age.median
+
+#clear missing values of fare
+fare.median <- median(titanic.full$Fare, na.rm = TRUE)
+titanic.full[is.na(titanic.full$Fare), "Fare"] <- fare.median
+
+#categorical casting
+titanic.full$Pclass <- as.factor(titanic.full$Pclass)
+titanic.full$Sex <- as.factor(titanic.full$Sex)
+titanic.full$Embarked <- as.factor(titanic.full$Embarked)
+
+#split dataset back out into train and set
+titanic.train <- titanic.full[titanic.full$IsTrainSet==TRUE,]
+titanic.test <- titanic.full[titanic.full$IsTrainSet==FALSE,]
+
+titanic.train$Survived <- as.factor(titanic.train$Survived)
+
+survived.equation <- "Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+survived.formula <- as.formula(survived.equation)
+install.packages("randomForest")
+library(randomForest)
+
+titanic.model <- randomForest(formula = survived.formula, data = titanic.train, ntree= 500, mtry = 3, nodesize = 0.01 * nrow(titanic.train))
+
+features.equation <- "Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+Survived <- predict(titanic.model, newdata = titanic.test)
+
+PassengerId <- titanic.test$PassengerId
+output.df <- as.data.frame(PassengerId)
+output.df$Survived <- Survived
+
+write.csv(output.df, file = "kaggle_submission.csv", row.names = FALSE)
